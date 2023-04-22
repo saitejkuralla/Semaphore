42179f08e7e9d773c8df0bcfef4d3ad3c8dd3359
Prathima Banda <prathima_banda@epam.com>
2023-04-22 23:14:48 +0530

    Thread Sync Methods Added

---
+++ Multi threading/Monitor_project/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+namespace MonitorExample
+{
+    public class BankAccount
+    {
+        private int balance;
+
+        public BankAccount(int initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+            Monitor.Enter(this);
+            try
+            {
+                Console.WriteLine($"Depositing {amount}...");
+                balance += amount;
+                Console.WriteLine($"New balance is {balance}");
+                Monitor.Pulse(this);
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            Monitor.Enter(this);
+            try
+            {
+                while (balance < amount)
+                {
+                    Console.WriteLine($"Withdrawal of {amount} blocked, waiting for deposit...");
+                    Monitor.Wait(this);
+                }
+                Console.WriteLine($"Withdrawing {amount}...");
+                balance -= amount;
+                Console.WriteLine($"New balance is {balance}");
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = 100;
+            var account = new BankAccount(balance);
+            Console.WriteLine($"Current available balance is:{balance}");
+            Thread t1 = new Thread(() =>
+            {
+                account.Withdraw(50);
+                Console.WriteLine("Withdrawal complete");
+            });
+            Thread t2 = new Thread(() =>
+            {
+                account.Deposit(100);
+                Console.WriteLine("Deposit complete");
+            });
+            t1.Start();
+            t2.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Multi threading/SemaphoreSlim_project2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreSlim_Project2
+{
+    internal class Program
+    {
+        static SemaphoreSlim semaphore = new SemaphoreSlim(5); // allow up to 5 concurrent connections
+        static string connectionString = "Data Source=localhost;Initial Catalog=Company;Integrated Security=True";
+
+
+        private static async Task<int> GetRowCountAsync()
+        {
+            await semaphore.WaitAsync(); // wait until there's a free connection
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand("SELECT COUNT(*) FROM TableName", connection))
+                {
+                    await connection.OpenAsync();
+                    return (int)await command.ExecuteScalarAsync();
+                }
+            }
+            finally
+            {
+                semaphore.Release(); // release the connection
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread Started");
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Task<int>[] tasks = new Task<int>[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tasks[i] = GetRowCountAsync();
+            }
+
+            await Task.WhenAll(tasks);
+
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Console.WriteLine($"Task {i + 1} Result: {tasks[i].Result}");
+            }
+
+            Console.WriteLine($"Main Thread Completed");
+            stopwatch.Stop();
+            Console.WriteLine($"Processing of queries done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+
+            Console.ReadKey();
+        }
+    }
+}
